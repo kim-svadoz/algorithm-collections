6b439a40205ceee4816811230e0b70636cff779b
kim_svadoz <dhkdghehfdl@gmail.com>
2020-02-25 17:20:37 +0900

    [DFS]백준 2667 단지번호붙이기

--- src/baekjoon/danji_2667.java
+++ src/baekjoon/danji_2667.java
@@ -1,46 +1,61 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class danji_2667 {
+	static int N, cnt;
+	static int[][] map;
+	static int[][] visited;
+	static int dx[] = {-1,1,0,0};
+	static int dy[] = {0,0,-1,1};
+	static ArrayList<Integer> my = new ArrayList<Integer>();	
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		int x= key.nextInt();
-		int[][] dan = new int[x][x];
-		for(int i=0; i<x; i++) {
-			for(int j=0; j<x; j++) {
-				dan[i][j] = key.nextInt();
+		N= key.nextInt();
+		
+		map = new int[N][N];
+		visited = new int[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String input = key.next();
+			for(int j=0;j<N;j++) {
+				map[i][j] = input.charAt(j)-'0';
 			}
 		}
 		
-		int cnt1 = 0;
-		for(int i=0; i<x; i++) {
-			for(int j=0; j<x; j++) {
-				if(dan[i][j] == 1) {
-					cnt1 ++;
-					if(dan[i][j+1] == 1) {
-						cnt1++;
-					}else if(dan[i+1][j] == 1) {
-						
-					}
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(map[i][j] == 1 && visited[i][j] ==0) {
+					cnt = 1;
+					solution(i,j);
+					my.add(cnt);
 				}
 			}
 		}
 		
-	}
-	
-	public int solution(int x, int a, int b) {
-		int count=0;
-		int[][] dan = new int[x][x];
-		if(dan[a][b] ==dan[a][b+1]) {
-			
+		Collections.sort(my);
+		System.out.println(my.size());
+		for(int i=0; i<my.size();i++) {
+			System.out.println(my.get(i));
 		}
 		
-		for(int i=0; i<x; i++) {
-			for(int j=0; j<x; j++) {
-				dan[a][b] = ////
+	}
+	
+	public static int solution(int x, int y) {
+		visited[x][y] = 1;
+		for(int i=0; i<4; i++) {
+			int nx = x+dx[i];
+			int ny = y+dy[i];
+			
+			if(nx>=0 && ny>=0 && nx<N && ny<N) {
+				if(map[nx][ny]==1 && visited[nx][ny]==0) {
+					solution(nx,ny);
+					cnt++;
+				}
 			}
 		}
-		return count;
-	}
+		return cnt;
+	}	
 }
