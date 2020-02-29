7e734adc46c2b9429cdfd6bd372b37703d574dc2
kim_svadoz <dhkdghehfdl@gmail.com>
2020-02-29 18:03:27 +0900

    baekjoon_1076(HashMap)

--- src/baekjoon/resistance_1076.java
+++ src/baekjoon/resistance_1076.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class resistance_1076 {
-
+//단순 HashMap을 이용한 put, get 사용법
 	public static void main(String[] args) {
 		
 		Scanner key = new Scanner(System.in);
@@ -21,12 +21,10 @@ public class resistance_1076 {
 		map.put("grey", "8,100000000");
 		map.put("white", "9,1000000000");
 		
-		
 		String first = key.next();
 		String second = key.next();
 		String third = key.next();
 		key.close();
-		
 		
 		String[] get1 = map.get(first).split(",");
 		String[] get2 = map.get(second).split(",");
